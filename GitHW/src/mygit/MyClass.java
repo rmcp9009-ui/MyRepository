45c8dd26db087c32aa2646b278fc1ba79100ad97
rmcp9 <rmcp9@ESTERN>
2025-12-14 17:16:24 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -1,6 +1,9 @@
 package mygit;
 public class MyClass {
-public static void main(String[] args) {
-System.out.println("Hello Git!");
+	public static void main(String[]args) {
+		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
+		YourClass your=new YourClass();
+		your.sayHello("Everyone");
 }
 }
