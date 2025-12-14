d26eb846de06dd1a8393817c186ec98e7b1697a2
rmcp9 <rmcp9@ESTERN>
2025-12-14 17:19:42 +0900

    change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -1,6 +1,8 @@
 package mygit;
 public class MyClass {
 public static void main(String[] args) {
-System.out.println("Hello Git!");
+	System.out.println("Hello Git!");
+	System.out.println("This is the 2nd version.");
+	System.out.println("My chaned in master branch");
 }
 }
--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -1,9 +1,11 @@
 package mygit;
 
 public class YourClass {
-	
 	public void sayHello(String toWhom) {
 		System.out.println("Hello! "+toWhom);
+		System.out.println("Your change in master branch");
 	}
+	
+	
 
 }
